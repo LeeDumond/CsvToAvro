c9e08e3a9f62a885f4f626887be15aa5bc16265b
Lee Dumond <leedumond@gmail.com>
2019-06-06 09:11:10 -0500

    separate basic and advanced usage demos in console app

--- CsvToAvroConsoleApp/Program.cs
+++ CsvToAvroConsoleApp/Program.cs
@@ -6,54 +6,66 @@ namespace CsvToAvroConsoleApp
 {
     class Program
     {
+        private static string schemaFilePath = @"C:\CsvToAvro\mySchema.avsc";
+        private static string csvFilePath = @"C:\CsvToAvro\myCsv.csv";
+        private static string outputFilePath = @"C:\CsvToAvro\myAvro.avro";
+
         static void Main(string[] args)
         {
-            string schemaFilePath = @"C:\CsvToAvro\mySchema.avsc";
-            string csvFilePath = @"C:\CsvToAvro\myCsv.csv";
-            string outputFilePath = @"C:\CsvToAvro\myAvro.avro";
+            // uncomment whichever scenario you wish to demonstrate below
 
+            BasicUsage();
+            //AdvancedUsage();
+        }
+
+        private static void BasicUsage()
+        {
             CsvToAvroGenericWriter writer = CsvToAvroGenericWriter.CreateFromPath(schemaFilePath, outputFilePath);
 
-            //// BASIC USAGE
             int counter = writer.ConvertFromCsv(csvFilePath);
 
-            // ADVANCED USAGE
+            Console.WriteLine($"There were {counter} lines processed from: {csvFilePath}");
+            Console.WriteLine($"The results were written to: {outputFilePath}");
+        }
 
-            //string jsonSchema = @"{
-            //  ""namespace"": ""com.leedumond"",
-            //  ""type"": ""record"",
-            //  ""name"": ""CostIndex"",
-            //  ""version"": ""1"",
-            //  ""fields"": [
-            //    { ""name"": ""medicare_administrative_contractor"", ""type"": ""string"" },
-            //    { ""name"": ""locality_number"", ""type"": ""string"" },
-            //    { ""name"": ""locality_name"", ""type"": ""string"" },
-            //    { ""name"": ""pw_gpci"", ""type"": ""float"" },
-            //    { ""name"": ""pe_gpci"", ""type"": ""float"" },
-            //    { ""name"": ""mp_gpci"", ""type"": [""float"", ""null""] }
-            //  ]
-            //}";
+        private static void AdvancedUsage()
+        {
+            string jsonSchema = @"{
+              ""namespace"": ""com.leedumond"",
+              ""type"": ""record"",
+              ""name"": ""CostIndex"",
+              ""version"": ""1"",
+              ""fields"": [
+                { ""name"": ""medicare_administrative_contractor"", ""type"": ""string"" },
+                { ""name"": ""locality_number"", ""type"": ""string"" },
+                { ""name"": ""locality_name"", ""type"": ""string"" },
+                { ""name"": ""pw_gpci"", ""type"": ""float"" },
+                { ""name"": ""pe_gpci"", ""type"": ""float"" },
+                { ""name"": ""mp_gpci"", ""type"": [""float"", ""null""] }
+              ]
+            }";
 
-            //CsvToAvroGenericWriter writer = CsvToAvroGenericWriter.CreateFromJson(jsonSchema, outputFilePath);
+            CsvToAvroGenericWriter writer = CsvToAvroGenericWriter.CreateFromJson(jsonSchema, outputFilePath);
 
-            //string[] headerFields = { "medicare_administrative_contractor", "locality_number", "locality_name", "pw_gpci", "pe_gpci", "mp_gpci" };
-            //writer.SetCsvHeader(headerFields);
+            string[] headerFields = { "medicare_administrative_contractor", "locality_number", "locality_name", "pw_gpci", "pe_gpci", "mp_gpci" };
+            writer.SetCsvHeader(headerFields);
 
-            //int counter = 0;
-            //using (var reader = new StreamReader(csvFilePath))
-            //{
-            //    string line;
-            //    while ((line = reader.ReadLine()) != null)
-            //    {
-            //        if (!string.IsNullOrEmpty(line))
-            //        {
-            //            writer.Append(line);
-            //            counter++;
-            //        }
-            //    }
-            //}
+            int counter = 0;
 
-            //writer.CloseWriter();
+            using (var reader = new StreamReader(csvFilePath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (!string.IsNullOrEmpty(line))
+                    {
+                        writer.Append(line);
+                        counter++;
+                    }
+                }
+            }
+
+            writer.CloseWriter();
 
             Console.WriteLine($"There were {counter} lines processed from: {csvFilePath}");
             Console.WriteLine($"The results were written to: {outputFilePath}");
--- README.md
+++ README.md
@@ -43,9 +43,11 @@ using(TextReader reader = new StringReader(myCsvString))
 
 You may also indicate how many header lines should be skipped in the CSV file (*default is 0*), and a custom separator (*default is comma*).
 
+This usage is capable of parsing complex CSV data including quoted lines, embedded newlines, escaped quotes, and the like; and is recommended for most scenarios.
+
 ### Advanced Usage
 
-If you need more control, you may parse the CSV yourself and use any of the `Append()` overloads to add the values from each line to the writer manually (don't forget to close the writer after you're done!):
+If you need more control, you may loop through your CSV data yourself and use any of the `Append()` overloads to add the values from each line to the writer manually (don't forget to close the writer after you're done!):
 
 ```C#
 int counter = 0;
@@ -68,7 +70,7 @@ Console.WriteLine($"There were {counter} lines processed from: {csvFilePath}");
 ```
 The `Append()` methods accept either an array of string values, or a raw separated string. 
 
-Note that the `Append(string line, char separator = DEFAULT_SEPARATOR)` overload makes no attempt to perform any complex parsing; it simply splits the string based on the supplied separator (the default is comma). If your line data has quoted values, newlines, escaped quotes, etc. you should use the `Append(string[] fields)` overload instead.
+Note that the `Append(string line, char separator = DEFAULT_SEPARATOR)` overload makes no attempt to perform any complex parsing; it simply splits the string based on the supplied separator (the default is comma). If your line data has quoted values, newlines, escaped quotes, etc. you should parse the data yourself and use the `Append(string[] fields)` overload, or consider using the `ConvertFromCsv` method as explained above.
 
 ### Dealing with Field Ordering
 
